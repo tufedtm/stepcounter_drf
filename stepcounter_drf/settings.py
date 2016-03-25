2c470c631071d3da36ebfbb7abfc8ee9d5352041
tufedtm <tufedtm@gmail.com>
2016-03-25 14:39:27 +0300

    + миграции + ограничение типов запросов для анонимов

--- stepcounter_drf/settings.py
+++ stepcounter_drf/settings.py
@@ -109,12 +109,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    # ],
-    # 'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ),
     'PAGE_SIZE': 10
 }
 
